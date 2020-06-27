e026ba3fb253f11e43c95938f806945f8ee317ca
Harshana Serasinghe <harshanadabezt@gmail.com>
2020-06-27 15:31:45 +0530

    Initial Commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend';
+  
+  add(n1, n2) {    
+    return n1+n2;   
+  }
+}
